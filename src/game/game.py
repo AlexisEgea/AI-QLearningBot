43d125ab6e08f6852e4100ab06485fc7889daf6c
AlexisEgea <alexisegea@outlook.com>
2025-01-11 02:22:27 +0100

    Updating start_game method to use more than one player + Adding winning method and so on (not finished yet)

--- src/game/game.py
+++ src/game/game.py
@@ -1,88 +1,126 @@
 import json
 import os
-import random
+
+from src.game.grid.grid import Grid
 
 
 class Game:
     def __init__(self):
+        self.player_ids = []
+        self.grid = None
+
         self.state = None
         self.action = None
-        self.horizon = None
 
-        self.played_party = 0
-        self.win_game = 0
-        self.loose_game = 0
+        self.init_game()
+
+        self.played_party = [0 for _ in range(len(self.player_ids))]
+        self.win_game = [0 for _ in range(len(self.player_ids))]
+        self.loose_game = [0 for _ in range(len(self.player_ids))]
+
 
     def get_state(self):
         pass
 
-    def get_horizon(self):
-        pass
 
     def retrieve_all_actions(self):
         pass
 
     # Init from specific configuration
     def init_game(self):
-        pass
+        config_path = os.path.join(os.getcwd(), "configuration/config.json")
+        with open(config_path, 'r') as file:
+            data = json.load(file)
+
+        num_player = data['number_player']
+        self.player_ids = [i+1 for i in range(num_player)]
+
+        self.grid = Grid()
+        self.grid.init_players(self.player_ids)
+
 
     def reset_game(self):
-        pass
+        self.grid = Grid()
+        self.grid.init_players(self.player_ids)
+
 
     # Init all possible actions
     def init_actions(self):
         pass
 
-    def play_action(self, action):
-        pass
+
+    def play_action(self, id, action):
+        cell = self.grid.get_player(id)
+        return self.grid.play_action(cell, action)
+
 
     def get_score(self):
         pass
 
 
-    def start_game(self, player, number_party):
+    def start_game(self, players, number_party):
+        for i in range(len(players)):
+            players[i].set_id(self.player_ids[i])
+
         for i in range(0, number_party):
-            print(f"Game {i+1} ____________________")
-            # TODO: Add the first action depending on the game or remove the 3 next lines
-            action = None
-            self.play_action(action)
-            self.score_state()
+            print(f"Game {i + 1} ____________________")
+            current_player_index = 0
             while not self.end():
-                player.perceive(self)
-                action = player.decide()
-                self.play_action(action)
+                print(f" Player {players[current_player_index].id}")
+                current_player = players[current_player_index]
+                current_player.perceive()
+
+                good_action = False
+                while not good_action:
+                    action = current_player.decide()
+                    good_action = self.play_action(players[current_player_index].id, action)
+                print(self.grid)
+
                 result = self.score_state()
-                player.sleep(result)
+                current_player.sleep(result)
+                current_player_index = (current_player_index + 1) % len(players)
+
             self.reset_game()
+
         print(f"On {self.played_party} games, {self.win_game} were won and {self.loose_game} were lost")
         # TODO: result_games is the average result of the played parties and won parties
         result_games = 0
         print(f"score: {result_games}")
 
+
     def score_state(self):
         return 0
 
-    def win(self):
-        # Check if the win condition of the game is met.
-        # TODO: Implement the logic to determine if the player has won.
-        if True:  # Replace 'True' with the actual win condition
-            print("Game Win !!!")
-            self.win_game += 1
-            return True
-        return False
 
-    def loose(self):
-        # Check if the lose condition of the game is met.
-        # TODO: Implement the logic to determine if the player has lost.
-        if True:  # Replace 'True' with the actual lose condition
-            print("Game Loose :(")
-            self.loose_game += 1
-            return True
-        return False
+    def win(self):
+        grid_size = (len(self.grid.tab) + 1) // 2
+        player_position = []
+        for id in self.player_ids:
+            cell = self.grid.get_player(id)
+            player_position.append(cell)
+
+        for player in player_position:
+            if player.id == 1 and player.x == grid_size * 2 - 2:  # Player 1 reaches the last row
+                return player.id
+            if player.id == 2 and player.x == 0:  # Player 2 reaches the first row
+                return player.id
+            if player.id == 3 and player.y == grid_size * 2 - 2:  # Player 3 reaches the last column
+                return player.id
+            if player.id == 4 and player.y == 0:  # Player 4 reaches the first column
+                return player.id
+
+        return 0
+
 
     def end(self):
-        if self.win() or self.loose():
-            self.played_party +=1
+        winner = self.win()
+        if winner != 0:
+            for id in self.player_ids:
+                if id == winner:
+                    self.win_game[id - 1] += 1
+                else:
+                    self.loose_game[id - 1] += 1
+                self.played_party[id - 1] += 1
             return True
         return False
 
