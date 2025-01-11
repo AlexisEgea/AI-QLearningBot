37f59abf6a14013e8ec905f88907e33c0f8cc8bf
AlexisEgea <alexisegea@outlook.com>
2025-01-11 02:24:07 +0100

    Updating launcher human to use two players (for now)

--- src/launcher_human.py
+++ src/launcher_human.py
@@ -3,5 +3,9 @@ from player.human import Human
 
 if __name__ == '__main__':
     game = Game()
+    print(game.grid)
+
     human = Human()
-    game.start_game(human, 1)
+    robot = Human()
+
+    game.start_game([human, robot], 1)
