063640a1b75de794d7576cabc54de1be8cf4f442
AlexisEgea <alexisegea@outlook.com>
2025-02-17 20:16:22 +0100

    Adding specific file for evaluation of reward/score

---
+++ src/utils/evaluation.py
@@ -0,0 +1,6 @@
+class Evaluation:
+    # TODO: need to be implemented
+    # IDEA: reward = len(number cell player to win) - max(len(number cell oponents))
+    @classmethod
+    def score(cls, game):
+        return 0
