86c0b12bc466beae628712c07176db2705ccd7a2
AlexisEgea <alexisegea@outlook.com>
2025-02-17 20:15:39 +0100

    Remove start_game in a specific launcher file, same for score method

--- src/game/game.py
+++ src/game/game.py
@@ -12,33 +12,37 @@ class Game:
         self.state = None
         self.action = None
 
+        self.init_players()
         self.init_game()
 
         self.played_party = [0 for _ in range(len(self.player_ids))]
         self.win_game = [0 for _ in range(len(self.player_ids))]
         self.loose_game = [0 for _ in range(len(self.player_ids))]
+        self.score = [0 for _ in range(len(self.player_ids))]
+        self.sum_score = [0 for _ in range(len(self.player_ids))]
 
 
     def get_state(self):
         pass
 
-
     def retrieve_all_actions(self):
         pass
 
-    # Init from specific configuration
-    def init_game(self):
+    def init_players(self):
         config_path = os.path.join(os.getcwd(), "configuration/config.json")
         with open(config_path, 'r') as file:
             data = json.load(file)
 
         num_player = data['number_player']
-        self.player_ids = [i+1 for i in range(num_player)]
+        self.player_ids = [i + 1 for i in range(num_player)]
 
+    # Init from specific configuration
+    def init_game(self):
         self.grid = Grid()
         self.grid.init_players(self.player_ids)
 
 
+    # TODO: redefine players to change the order
     def reset_game(self):
         self.grid = Grid()
         self.grid.init_players(self.player_ids)
@@ -68,41 +72,6 @@ class Game:
         pass
 
 
-    def start_game(self, players, number_party):
-        for i in range(len(players)):
-            players[i].set_id(self.player_ids[i])
-
-        for i in range(0, number_party):
-            print(f"Game {i + 1} ____________________")
-            current_player_index = 0
-            while not self.end():
-                print(f" Player {players[current_player_index].id}")
-                current_player = players[current_player_index]
-                current_player.perceive()
-
-                good_action = False
-                while not good_action:
-                    action = current_player.decide()
-                    good_action = self.play_action(players[current_player_index], action)
-
-                print(self.grid)
-
-                result = self.score_state()
-                current_player.sleep(result)
-                current_player_index = (current_player_index + 1) % len(players)
-
-            self.reset_game()
-
-        print(f"On {self.played_party} games, {self.win_game} were won and {self.loose_game} were lost")
-        # TODO: result_games is the average result of the played parties and won parties
-        result_games = 0
-        print(f"score: {result_games}")
-
-
-    def score_state(self):
-        return 0
-
-
     def win(self):
         player_position = [
             self.grid.get_player(id) for id in self.player_ids
@@ -118,6 +87,7 @@ class Game:
                         return player.id
 
         return 0  # No players win
+
 
     def end(self):
         winner = self.win()
